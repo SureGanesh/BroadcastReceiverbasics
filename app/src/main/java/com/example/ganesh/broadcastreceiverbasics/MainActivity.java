837552c78bbdb3f449b5b10f5524f012b32c73e9
kvana <kvana123>
2017-05-22 17:44:51 -0700

    br added in MainActivity

--- app/src/main/java/com/example/ganesh/broadcastreceiverbasics/MainActivity.java
+++ app/src/main/java/com/example/ganesh/broadcastreceiverbasics/MainActivity.java
@@ -1,14 +1,33 @@
 package com.example.ganesh.broadcastreceiverbasics;
 
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    private int MY_PERMISSION;
+    //private int MY_PERMISSION;
+
+    private BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        broadcastReceiver = new MyBroadcastReceiver();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        IntentFilter intentFilter = new IntentFilter("android.provider.Telephony.SMS_RECEIVED");
+        registerReceiver(broadcastReceiver,intentFilter);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(broadcastReceiver);
     }
 }
